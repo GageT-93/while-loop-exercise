eebac3ed224f418935b4a3eb1b6d329552c10720
MacMini-19 <macmini-19@MacMini-19s-Mac-mini.local>
2022-03-29 11:54:42 -0700

    first commit

---
+++ exercises.js
@@ -0,0 +1,40 @@
+// let i = 0;
+
+// while (i < 9) {
+//     i++;
+//     console.log(i)
+// }
+
+// let i = 10;
+
+// while (i > 1) {
+//     i--;
+//     console.log(i)
+// }
+
+// let num = 10000;
+
+// while (num > 10) {
+//     num /= 2;
+//     console.log(num)
+// }
+
+// let i = 0;
+
+// while (i < 12) {
+//     i++;
+//     if (i === 3) {
+//         continue;
+//     }
+//     console.log(i)
+// }
+
+let i = 0;
+
+while (i < 12) {
+    i++;
+    if (i === 3 || i === 6) {
+        continue;
+    }
+    console.log(i)
+}
